dafe52f5339eea45b7414ebc51ea50d786c2c642
aashishshrestha09 <54453596+aashishshrestha09@users.noreply.github.com>
2020-12-10 23:14:50 -0500

    17. Scroll Animations

--- src/components/Toogle.js
+++ src/components/Toogle.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import {motion} from 'framer-motion'
 
 export default function Toogle({children, title}) {
-    const [toogle, setToogle] = useState(true);
+    const [toogle, setToogle] = useState(false);
     return (
         <motion.div layout className="question" onClick={() => setToogle(!toogle)}>
             <motion.h4 layout>{title}</motion.h4>
